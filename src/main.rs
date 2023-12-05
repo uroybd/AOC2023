9b5ac01ed8f45eba7063ef37f35432ef936b8ebb
Utsob Roy <roy@utsob.me>
2023-12-05 20:09:15 +0600

    chore: 🗑️ cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use aoc_2023::solutions::day05::solution_day_05_02;
-
 fn main() {
-    let file_path: String = String::from("src/inputs/day05.txt");
-    let result = solution_day_05_02(file_path);
-    println!("{:?}", result.unwrap())
+    println!("Hello AOC2023!")
 }
